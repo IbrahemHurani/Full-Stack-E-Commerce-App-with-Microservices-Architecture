c1fa46160572dfdd1493f05a1e77cb5cfb94ef2d
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2026-05-30 09:38:24 +0300

    startSever method added

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,11 @@ if (ENV.NODE_ENV === "production") {
 
     });
 }
-app.listen(ENV.PORT, () => {
-    console.log("server is up and running");
-    connectDB();
-});
+
+const startServer = () => {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+        console.log("server is up and running");
+    });
+};
+startServer();
